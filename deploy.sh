56c12a48cd74e7947c24600e1acb4a60c8b30aad
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-11-19 21:43:57 +0100

    automatic site update

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 jekyll build
+git add .
+git commit -m "automatic site update"
+git push --mirror origin
 rsync -Pahv --exclude=files -exclude=.well-known --delete-after _site/ ../../../public/palinuro/
+rsync -Pahv --exclude=files -exclude=.well-known --delete-after _site/ ../../palinuro,kbpbot/website/
